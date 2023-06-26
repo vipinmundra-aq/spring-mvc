7a1bdefa94716090eee98ae385612a5d2c41c64d
vipin mundra <vipin.mundra@teramatrix.local>
2023-06-26 12:53:38 +0530

    spring-mvc-test

---
+++ src/main/java/mycompany/user/UserRepository.java
@@ -0,0 +1,7 @@
+package mycompany.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    public Long countById(Integer id);
+}
